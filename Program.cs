c5b40218ab7bc14f60e336adf443250c72485fc8
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-01 01:20:16 +0200

    snack1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
+            //Snack1. L’utente inserisce due numeri in successione. Il software stampa il maggiore.
 
+            //Console.WriteLine("Inserisci il primo numero");
+            //int x = System.Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Inserisci il secondo numero");
+            //int y = System.Convert.ToInt32(Console.ReadLine());
+
+            //if (x > y)
+            //{ Console.WriteLine(x); }
+            //else
+            //{ Console.WriteLine(y); }
 
         }
     }
