aac019494d5e1969088f62225a65ea876c3e3f81
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-01 01:20:47 +0200

    snack 3

--- Program.cs
+++ Program.cs
@@ -33,6 +33,23 @@
             //    Console.WriteLine(x);
             //    Console.WriteLine(y);
             //}
+
+            ////Snack3.Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+            //List<int> numeri = new List<int>();
+
+            //int somma = 0;
+            //for (int i = 0; i < 10;  i++)
+            //{
+            //    Console.WriteLine("Inserisci un numero");
+            //    int numero = Convert.ToInt32(Console.ReadLine());
+            //    numeri.Add(numero);
+            //}
+            //foreach (int numero in numeri)
+            //{
+            //    somma += numero; 
+            //}
+            //Console.WriteLine(somma);
+
         }
     }
 }
